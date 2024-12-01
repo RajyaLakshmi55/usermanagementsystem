733c3924b590407154f40404b1e38fd3ebcd6782
RajyaLakshmi55 <162141132+RajyaLakshmi55@users.noreply.github.com>
2024-12-01 11:50:18 +0530

    UserManagementSystem

---
+++ usermanagementsystem/src/main/java/com/userdata/usermanagementsystem/repository/UsersRepo.java
@@ -0,0 +1,12 @@
+package com.userdata.usermanagementsystem.repository;
+
+
+import com.userdata.usermanagementsystem.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
+
+    Optional<OurUsers> findByEmail(String email);
+}
